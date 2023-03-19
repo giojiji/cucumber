864e6c9ed38bf3fd53a28f5f558d4141a0dd0021
giojiji <giojiji923@gmail.com>
2023-03-19 17:12:04 +0400

    test is done

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-create javascript file
+let user;
+let pass;
+let statusacces = document.getElementById("status")
+
+function enteruser() {
+user = document.getElementById("user")
+pass = document.getElementById("pass")
+
+if(user.value == "giorgi" && pass.value == "1234") {
+    statusacces.innerText = "you are wellcome"
+} else if (user.value.includes("@")) {
+    statusacces.innerText = "inccorect format"
+}
+
+else {
+    statusacces.innerText = "your password is wrong"
+}
+
+
+}
